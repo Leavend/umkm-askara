351e27c734447cda8e13655cd1862f813e4aa6f9
Tio Hady Pranoto <76149973+Leavend@users.noreply.github.com>
2023-06-17 07:21:33 +0800

    Update products.js

--- pages/products.js
+++ pages/products.js
@@ -20,7 +20,7 @@ export default function Products() {
       <table className="basic mt-2">
         <thead>
           <tr>
-            <td>Product name</td>
+            <td>Nama Produk</td>
             <td></td>
           </tr>
         </thead>
