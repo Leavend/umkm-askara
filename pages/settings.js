9eac251a21ba90c2e45c97973dbb6e04b48871a2
Tio Hady Pranoto <76149973+Leavend@users.noreply.github.com>
2023-06-17 07:23:12 +0800

    Update settings.js

--- pages/settings.js
+++ pages/settings.js
@@ -60,13 +60,13 @@ function SettingsPage({swal}) {
               <option key={product._id} value={product._id}>{product.title}</option>
             ))}
           </select>
-          <label>Shipping price (in usd)</label>
+          <label>Ongkos Kirim (Flat)</label>
           <input type="number"
                  value={shippingFee}
                  onChange={ev => setShippingFee(ev.target.value)}
           />
           <div>
-            <button onClick={saveSettings} className="btn-primary">Save settings</button>
+            <button onClick={saveSettings} className="btn-primary">Simpan</button>
           </div>
         </>
       )}
