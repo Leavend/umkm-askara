25eaa14ad582a43d24006d6dce495c2d16d509fc
Tio Hady Pranoto <76149973+Leavend@users.noreply.github.com>
2023-06-17 07:22:10 +0800

    Update orders.js

--- pages/orders.js
+++ pages/orders.js
@@ -15,14 +15,14 @@ export default function OrdersPage() {
   }, []);
   return (
     <Layout>
-      <h1>Orders</h1>
+      <h1>Pesanan</h1>
       <table className="basic">
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Paid</th>
-            <th>Recipient</th>
-            <th>Products</th>
+            <th>Tanggal</th>
+            <th>Pembayaran</th>
+            <th>Struk</th>
+            <th>Produk UMKM</th>
           </tr>
         </thead>
         <tbody>
